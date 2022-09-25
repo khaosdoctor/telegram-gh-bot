1a67c4e214a7948e055d3c73c59406f008cd27b1
Lucas Santos <lhs.santoss@gmail.com>
2022-09-26 00:17:45 +0200

    fix: update sift
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 import { webhookCallback } from 'x/grammy@v1.11.0/mod.ts'
-import { serve } from 'x/sift@0.5.0/mod.ts'
+import { serve } from 'x/sift@0.6.0/mod.ts'
 import { getBot } from './bot.ts'
 import { config } from './config.ts'
 const bot = getBot(config)
